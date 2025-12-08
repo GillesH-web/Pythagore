0dbe3feb37555a53a765e2d9ab073564e8318cf9
GillesH-web <gilles.hestin@gmail.com>
2025-12-08 10:13:02 +0100

    Add files via upload
    Signed-off-by: GillesH-web <gilles.hestin@gmail.com>

--- js/version-manager.js
+++ js/version-manager.js
@@ -6,7 +6,7 @@
 class VersionManager {
     constructor() {
         this.currentVersion = '1.9.1';
-        this.buildTimestamp = '2025-12-07 17:17';
+        this.buildTimestamp = '2025-12-08 10:11';
         this.init();
     }
 
